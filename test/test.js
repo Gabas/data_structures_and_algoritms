dbf9f894b5ee7757513f493a2cba7e392ef40e7a
Roman Gusev <Gabas@Romans-MacBook-Pro.local>
2015-09-07 18:14:29 -0700

    Implemented push, pop, shift, unshift

---
+++ test/test.js
@@ -0,0 +1,9 @@
+'use strict';
+
+var expect = require('chai').expect;
+
+describe("main_module_test", function() {
+  it("should read bitmap file and write new bitmap file", function() {
+    //
+  });
+});
